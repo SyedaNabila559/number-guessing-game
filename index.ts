5062843d9d4fe76793bac91154fa66bfe7d96040
Syeda Nabila <161962167+SyedaNabila559@users.noreply.github.com>
2024-03-25 10:22:23 -0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("Welcome to code with nabila - cli number guessing game");
+
+const randomNumber = Math.floor (Math.random()  * 5 + 1);
+
+const answer = await inquirer.prompt([
+{
+name: "userGuessdNumber",
+type:"number",
+message: "Enter your guess (number limit from 1 to 10)",
+},
+
+])
+  if(answer.userGuessdNumber === randomNumber){
+console.log("Congratulation! you guess a correct number");
+  }
+    else{
+        console.log("Sorry! you gusse a wrong number");
+    }
